bb5cc97f5a1008e9b6e12facd0c9704ddd2baec1
Roy Duenas <roy.duenas.sdtpnoli@gmail.com>
2024-12-09 19:23:46 +0800

    Width of book

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -24,7 +24,7 @@ export default function Home(){
             <div className="p-2 md:p-4 max-w-[100vw]">
                 <HTMLFlipBook 
                     className="mx-auto relative"
-                    width={400}
+                    width={350}
                     height={733}
                     maxShadowOpacity={0.5}
                     mobileScrollSupport={true}
