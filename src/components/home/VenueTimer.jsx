e003bb3ced91c535a46357e748bf88f52653ff50
Roy Duenas <roy.duenas.sdtpnoli@gmail.com>
2024-12-10 12:58:32 +0800

    Updated What to wear page

--- src/components/home/VenueTimer.jsx
+++ src/components/home/VenueTimer.jsx
@@ -4,14 +4,19 @@ export default function VenueTimer(){
     return(
         <div className="flex bg-black justify-center items-center h-full w-full p-4 text-accent">
             <div className="text-center flex flex-col gap-2">
-                <div className="uppercase">Join us as we celebrate the beginning of our new life together.</div>
+                <div className="text-[15pt] text-script">
+                    <div>Join us as we celebrate the beginning of our new life together.</div>
+                </div>
                 <FlipCountdown 
                     theme="dark"
                     hideYear
                     titlePosition='bottom'
                     endAt={'2025-01-11 14:00:00'}/>
-                <div className="uppercase">On Friday, The 11<sup>th</sup> of January 2025</div>
-                <div className="uppercase">At two o'clock in the afternoon</div>
+                <div className="text-[9pt]">
+                    <div className="uppercase">On Friday, The 11<sup>th</sup> of January 2025</div>
+                    <div className="uppercase">At two o'clock in the afternoon</div>
+                    <div className="uppercase"><strong>San Isidro Labrador Parish</strong>, Talay, Dumaguete City, Negros Oriental</div>
+                </div>
             </div>
         </div>
     )
--- src/components/home/WhatToWear.jsx
+++ src/components/home/WhatToWear.jsx
@@ -1,28 +1,41 @@
 export default function WhatToWear(){
     return(
-        <div className="p-8 text-secondary">
-            <div className="bg-accent"></div>
-            <p className="text-script text-3xl border-b border-accent/50 pb-2">What to wear</p>
-            <p className="font-bold my-2 text-center">For Sponsors</p>
-            <div className="items-center mt-4 text-center flex flex-col gap-4">
-                <div>
-                    <p className="font-bold">Boys</p>
-                    <div>
-                        <p>Long-sleeved <strong>Beige</strong> Barong</p>
-                        <p><strong>Black</strong> Slacks</p>
-                    </div>
-                </div>
-                <div>
-                    <p className="font-bold">Girls</p>
-                    <p><strong>Beige</strong> long dress</p>
-                </div>
-                <div>
-                </div>
+        <div className="p-8 text-secondary flex flex-col h-full">
+            <div>
+                <p className="text-script text-3xl border-b border-accent/50 pb-2">What to wear</p>
             </div>
-            <div className="p-4 text-center">
-                <hr className="border border-accent/20" />
-                <p className="font-bold my-2">For Guests</p>
-                <p>Long-sleeved polo, and khaki Pants</p>
+            <div className="flex-grow flex justify-center flex-col">
+                <p className="font-bold my-4">For Sponsors</p>
+                <div className="border-b border-accent/50 pb-4">
+                    <ul className="list-disc pl-5">
+                        <li>
+                            <p className="font-bold">Boys:</p>
+                            <ul className="list-[circle] pl-5">
+                                <li><p>Long-sleeved <strong>Beige</strong> Barong</p></li>
+                                <li><p><strong>Black</strong> Slacks</p></li>
+                            </ul>
+                        </li>
+                        <li>
+                            <p className="font-bold">Girls:</p>
+                            <ul className="list-[circle] pl-5">
+                                <li><p><strong>Beige</strong> long dress</p></li>
+                            </ul>
+                        </li>
+                    </ul>
+                </div>
+                <div className="">
+                    <div className="flex gap-2 justify-center mt-4">
+                        <div className="h-7 aspect-square rounded-full bg-[#a07a63]"></div>
+                        <div className="h-7 aspect-square rounded-full bg-[#683f21]"></div>
+                        <div className="h-7 aspect-square rounded-full bg-[#33180f]"></div>
+                        <div className="h-7 aspect-square rounded-full bg-[#22110a]"></div>
+                    </div>
+                    <p className="font-bold my-4">For Guests</p>
+                    <ul className="list-disc pl-5">
+                        <li><p>Long-sleeved polo, and khaki Pants</p></li>
+                        <li><p>Long Dress</p></li>
+                    </ul>
+                </div>
             </div>
         </div>
     )
