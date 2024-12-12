5a9f96a8b64f5719b22815119397e0c545637e0b
Roy Duenas <roy.duenas.sdtpnoli@gmail.com>
2024-12-12 10:36:04 +0800

    updates

--- src/components/home/CoverPage.jsx
+++ src/components/home/CoverPage.jsx
@@ -1,9 +1,9 @@
 export default function CoverPage(){
     return(
         <div className="h-full w-full relative">
-            <div className="absolute w-full h-full bg-cover bg-center -z-[4]" 
+            <div className="absolute w-full h-full bg-cover bg-center" 
             style={{backgroundImage: "linear-gradient(to bottom, #11070369, #A7795E5E), url(./assets/images/grass.jpg)"}}></div>
-            <div className="flex flex-col justify-between items-center h-full py-24">
+            <div className="flex flex-col justify-between items-center h-full py-24 relative z-50">
                 <div>
                     <div className="text-center">
                         <p className="text-8xl text-primary font-bold leading-10">Jhonna</p>
